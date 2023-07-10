bc188b8e95747f8f1c12d40fc3c2b4cfd75819ff
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-07-10 08:51:53 +0530

    [Rohit]Add: // add all files

---
+++ Regex_Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexNew regexNew = new RegexNew();
+            regexNew.MyRegex();
+        }
+    }
+}
---
+++ Regex_Demo/RegexNew.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Demo
+{
+    // a Regex ( regular Expression) is a pattern that is used to check whether a give string matches that
+    public class RegexNew
+    {
+        static string pattern = "^cen$";
+        public void MyRegex()
+        {
+            // we have created object of class
+            // we have a parameter pattern in given class constructor
+            Regex regex = new Regex(pattern);
+            // is match return true if its mateches the string otherwise it return flase
+            if (regex.IsMatch("Rohya"))
+            {
+                Console.WriteLine("string is match the pattern");
+            }
+            else
+            {
+                Console.WriteLine("string does not match the pattern");
+            }
+
+        }
+    }
+}
